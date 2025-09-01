dbd0b469bd9360d860d1ac1a25e274f03229e972
= <=>
2025-09-01 11:19:29 +0100

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module io.dream.algoedit {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens io.dream.algoedit to javafx.fxml;
+    exports io.dream.algoedit;
+}
